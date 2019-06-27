532d0274b5db3e0f970c5dd3db35dd9d8f2d0774
Samuel Gibson <sgibber2018@gmail.com>
2019-06-27 14:50:20 -0700

    added '.' as a key option to 'wait'

--- Legend/input_handlers.py
+++ Legend/input_handlers.py
@@ -40,7 +40,7 @@ def handle_player_turn_keys(key):
         return {'move': (-1, 1)}
     elif key_char == 'n':
         return {'move': (1, 1)}
-    elif key_char == 'z':
+    elif key_char == 'z' or key_char == '.':
         return {'wait': True}
 
     if key_char == 'g':
