0b62dabb8007e0221ffe13a2114356e88440dc23
Samuel Gibson <sgibber2018@gmail.com>
2019-07-12 08:37:16 -0700

    refactored setup_menu.py to be clearer and more future-proof

--- Legend/setup_menu.py
+++ Legend/setup_menu.py
@@ -1,59 +1,87 @@
+""" This script is just a little shell menu to get the desired options for the game from
+the player before launching. It determines the user's OS as being either Linux-y or Windows-y 
+before entering the command to launch the tcod program. """
 
 import os
 
-if os.sep == '/':
+def win_and_linux_reset():
+    """ resets the terminal window for a Linux or Windows system """
 
-    os.system("reset")
+    if os.sep == '/':
 
-elif os.sep == '\\':
+        os.system("reset")
 
-    os.system("cls")
+    elif os.sep == '\\':
 
-print("LEGENDARY ROGUELIKE\n")
-print("(P)lay\n")
-print("Play with (s)mall font\n")
-print("Play with (l)arge font\n")
-print("(Q)uit\n")
+        os.system("cls")
 
-launching_game = "echo Good-bye!"
+def display_setup_menu():
+    """ Display the interactive menu for the player """
+
+    print("LEGENDARY ROGUELIKE\n")
+    print("(P)lay\n")
+    print("Play with (s)mall font\n")
+    print("Play with (l)arge font\n")
+    print("(Q)uit\n")
+
+def win_and_linux_launch_str(font=None):
+    """ gets the launch string for the desired options and returns it """
+
+    if font:
+
+        if font == "small":
+
+            if os.sep == '/':
+        
+                return "python3 engine.py --font=small"
+
+            elif os.sep == '\\':
+
+                return "py engine.py --font=small"
+
+        elif font == "large":
+
+            if os.sep == '/':
+
+                return "python3 engine.py --font=large"
+
+            elif os.sep == '\\':
+
+                return "py engine.py --font=large"
+
+    if os.sep == '/':
+
+        return "python3 engine.py"
+
+    elif os.sep == '\\':
+
+        return "py engine.py" 
+
+win_and_linux_reset()
+
+display_setup_menu()
+
+launch_str = "echo Good-bye!"
 
 while True:
 
     user_input = input("~: ")
 
     if user_input.upper() == 'P':
-       
-        if os.sep == '/':
-
-            launching_game = "python3 engine.py"
-
-        elif os.sep == '\\':
- 
-            launching_game = "py engine.py" 
+      
+        launch_str = win_and_linux_launch_str()
 
         break
 
     elif user_input.lower() == 's':
 
-        if os.sep == '/':
-
-            launching_game = "python3 engine.py --font=small"
-
-        elif os.sep == '\\':
- 
-            launching_game = "py engine.py --font=small" 
+        launch_str = win_and_linux_launch_str(font="small")
 
         break
         
     elif user_input.lower() == 'l':
 
-        if os.sep == '/':
-
-            launching_game = "python3 engine.py --font=large"
-
-        elif os.sep == '\\':
- 
-            launching_game = "py engine.py --font=large" 
+        launch_str = win_and_linux_launch_str(font="large")
 
         break
 
@@ -65,4 +93,4 @@ while True:
 
         print("Invalid input! Try again.\n")
 
-os.system(launching_game)
+os.system(launch_str)
